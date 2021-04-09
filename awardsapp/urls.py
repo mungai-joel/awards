cdb2978b840ef831265c68467cfaf5e58288fda7
mungai-joel <mungaijoel123@gmail.com>
2021-04-09 09:29:15 +0300

    added urls for easy nav

---
+++ awardsapp/urls.py
@@ -0,0 +1,33 @@
+"""awardsapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url,include
+from django.contrib import admin
+from django.contrib.auth import views 
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('awards.urls')),
+    # url(r'^accounts/', include('registration.backends.simple.urls')),
+    # url(r'^logout/$', views.logout, {"next_page": '/'}),
+    # url(r'logout/', auth_views.logout.as_view()),
+    # url(r'^api-token-auth/', obtain_auth_token),
+    # url(r'^accounts/', include('django_registration.backends.activation.urls')),
+    # path(r'^accounts/', include('django.contrib.auth.urls')),
+]
+
+
